b6555520d93a7650ba3b8f1be85cb01be9d4d216
Michael Nguyen <michaelalexnguyen@gmail.com>
2019-03-03 19:16:45 -0500

    changed router

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,19 @@ import Header from "./components/header";
 import Projects from "./components/projects";
 import { library } from "@fortawesome/fontawesome-svg-core";
 import { faHome } from "@fortawesome/free-solid-svg-icons";
-import { BrowserRouter, Route } from "react-router-dom";
+import { HashRouter, Route } from "react-router-dom";
 
 import "./App.css";
 library.add(faHome);
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>
+      <HashRouter>
         <div>
           <Route path={"/"} component={Header} exact />
           <Route path={"/projects"} component={Projects} />
         </div>
-      </BrowserRouter>
+      </HashRouter>
     );
   }
 }
