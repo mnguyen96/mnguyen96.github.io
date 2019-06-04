46f6df4cd44bec4ba9e264cd97db16da81b20ed3
Michael <michael@Michaels-iMac.fios-router.home>
2019-06-04 18:12:38 -0400

    mobile styles

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -7,40 +7,44 @@ export default class Header extends Component {
     return (
       <div className="about">
         <div className="about" id="console">
-          <p>
-            &gt; Print michael.nguyen
+          <div className="contents">
+            <p>
+              &gt; Print michael.nguyen
+              <br />
+              <span className="term">
+                {" "}
+                &nbsp;&nbsp;=&gt; "Hi! I'm a software developer living
+                Maryland!"
+              </span>
+            </p>
+            &gt; michael.interests()
             <br />
             <span className="term">
-              {" "}
-              &nbsp;&nbsp;=&gt; "Hi! I'm a software developer living Maryland!"
+              &nbsp;&nbsp;=&gt; ["Programming", "Videography", "Design",
+              "Music"]
             </span>
-          </p>
-          &gt; michael.interests()
-          <br />
-          <span className="term">
-            &nbsp;&nbsp;=&gt; ["Programming", "Videography", "Design", "Music"]
-          </span>
-          <br />
-          <br />
-          &gt; michael.education()
-          <br />
-          <span className="term">
-            &nbsp;&nbsp;=&gt; "Computer Science graduate from University of
-            Maryland, College Park"
             <br />
             <br />
-          </span>
-          &gt; michael.projects()
-          <br />
-          <span className="term">
-            &nbsp;&nbsp;=&gt;{" "}
-            <Link to="/projects" id="link">
-              "Here are my personal projects!"
-            </Link>
+            &gt; michael.education()
             <br />
-          </span>
-          <br />
-          &gt; _
+            <span className="term">
+              &nbsp;&nbsp;=&gt; "Computer Science graduate from University of
+              Maryland, College Park"
+              <br />
+              <br />
+            </span>
+            &gt; michael.projects()
+            <br />
+            <span className="term">
+              &nbsp;&nbsp;=&gt;{" "}
+              <Link to="/projects" id="link">
+                "Here are my personal projects!"
+              </Link>
+              <br />
+            </span>
+            <br />
+            &gt; _
+          </div>
         </div>
       </div>
     );
