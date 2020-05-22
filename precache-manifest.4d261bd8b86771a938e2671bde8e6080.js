8eca90895a21d4ae322127f3d969ae185947b1d4
Michael Nguyen <michaelalexnguyen@gmail.com>
2020-05-21 20:17:28 -0400

    Updates

---
+++ precache-manifest.4d261bd8b86771a938e2671bde8e6080.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "96ee1dad9d58ff12a4a9",
+    "url": "/static/js/main.96ee1dad.chunk.js"
+  },
+  {
+    "revision": "3e43aa643f0f2296bd0c",
+    "url": "/static/js/2.3e43aa64.chunk.js"
+  },
+  {
+    "revision": "96ee1dad9d58ff12a4a9",
+    "url": "/static/css/main.d7be52ee.chunk.css"
+  },
+  {
+    "revision": "3e43aa643f0f2296bd0c",
+    "url": "/static/css/2.035af964.chunk.css"
+  },
+  {
+    "revision": "67dd060ad25963eedeed7ffa2f3065ff",
+    "url": "/index.html"
+  }
+];
